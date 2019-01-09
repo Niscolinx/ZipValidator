d5c385d194f3bff2d43c874bbc17526c238bb3bb
Niscolinx <35029903+Niscolinx@users.noreply.github.com>
2019-01-09 14:41:05 +0100

    The ZipValidator files

---
+++ script.js
@@ -0,0 +1,22 @@
+//when the page loades, the modal should open
+function openPopup() {
+  window.location.hash = 'openModal';
+}
+window.onload = openPopup;
+
+
+let input = document.querySelector("#name")
+let submit = document.querySelector("#submit")
+let invalid = document.querySelector("#invalid")
+
+let zipcodes = [11111,22222,33333,44444];
+submit.addEventListener("click", function(){
+  //Grabbing the input and transforming it into a number
+  let code = zipcodes.includes(Number(input.value))
+  if(code){
+    window.location.replace("index2.html");
+  }else{
+    invalid.style.display = 'block'
+  }
+})
+
